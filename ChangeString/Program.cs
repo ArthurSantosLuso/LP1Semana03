db3cd0e9288da70f2fa19b4942468c855bc28cfc
Arthur Santos <arthurluiz.developer@gmail.com>
2025-03-05 14:33:58 +0000

    Exercicio 2 Resolvido.

---
+++ ChangeString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Pedir a string ao utilizador
+            Console.Write("String: ");
+            string palavra = Console.ReadLine();
+
+            // Pedir o carater da string a ser trocado
+            Console.Write("Caráter: ");
+            char caraterTrocar = char.Parse(Console.ReadLine());
+
+            // Trocar os caraters da string original
+            string palavraTrocada = palavra.Replace(caraterTrocar, 'x');
+
+            // Imprimir a sting com os caracters trocados
+            Console.WriteLine(palavraTrocada);
+        }
+    }
+}
